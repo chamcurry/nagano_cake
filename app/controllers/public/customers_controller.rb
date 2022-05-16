766f8664bc998df3c6d5e4e2d9d7a2348ef9926b
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-16 07:01:56 +0000

    [finish]ACV作成

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,16 @@
+class Public::CustomersController < ApplicationController
+  def syow
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,51 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  devise_for :admins
+  # 管理者側のルーティング
+  get "/admin" => "admin/homes#top"
+  namespace :admin do
+    resources :customers,only:[:index,:show,:edit,:update]
+  end
+  namespace :admin do
+    resources :genres,only:[:index,:create,:edit,:update]
+  end
+  namespace :admin do
+    resources :items,only:[:index,:new,:create,:show,:edit,:update]
+  end
+  namespace :admin do
+    resources :order_details,only:[:update]
+  end
+  namespace :admin do
+    resources :orders,only:[:show,:update]
+  end
+# 会員側ルーティング
+
+# homes
+  get "/" => "public/homes#top"
+  get "/about" => "public/homes#about"
+
+  # items
+  resources :items,only:[:index,:show]
+
+  # customers
+  get "/customers/my_page" => "customers#show"
+  get "/customers/edit" => "customers#edit"
+  patch "/customers"=> "customers#update"
+  get "/customers/unsubscribe" => "customers#unsubscribe"
+  patch "/customers/withdraw" => "customers#withdraw"
+
+  # cart_items
+  resources :cart_items,only:[:index,:update,:destroy,:create]
+  delete "/cart_items/destroy_all" => "cart_items#destroy_all"
+
+  # orders
+  resources :orders,only:[:new,:create,:index,:show]
+  get "/orders/complete" => "orders#complete"
+  post "/orders/confirm" => "orders#confirm"
+
+  # addresses
+  resources :addresses,only:[:index,:edit,:create,:update,:destroy]
+
+  # デバイス
+  devise_for :admins,path: 'admin'
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
