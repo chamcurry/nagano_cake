86e7729d4e8742b740c4d0a8543ae3cd03b392bb
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-18 11:39:37 +0000

    [almost]商品詳細

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,15 +1,15 @@
 class Admin::ItemsController < ApplicationController
   def index
-    @items =Item.page(params[:page])
+    @item =Item.new
+    @items =Item.all
   end
   def new
     @item =Item.new
   end
-
   def create
     @item =Item.new(item_params)
     @item.save
-    redirect_to admin_item_path(@item)
+    redirect_to admin_items_path
   end
 
   def show
@@ -23,6 +23,6 @@ class Admin::ItemsController < ApplicationController
   end
   private
   def item_params
-    params.require(:item).permit(:image,:name,:introduction,:genr_id,:price,:is_active)
+    params.require(:item).permit(:image,:name,:introduction,:price,:is_active)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_admin!, except: [:top]
+  
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
