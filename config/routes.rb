ab099850b0e6df03b6551479c6c7a376ca565e08
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-20 19:07:43 +0000

    [finish]deviseコントローラとビューの作成

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :orders,only:[:show,:update]
   end
-  
+
 # 会員側ルーティング
 # homes
   get "/" => "public/homes#top"
@@ -45,7 +45,12 @@ Rails.application.routes.draw do
   resources :addresses,only:[:index,:edit,:create,:update,:destroy]
 
   # デバイス
-  devise_for :admins,path: 'admin'
-  devise_for :customers
+  devise_for :customers,skip:[:passwords],controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  devise_for :admin,skip:[:registrations,:passwords],controllers: {
+  sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
