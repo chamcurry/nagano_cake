acec7c371fd4be00ee709e9f5f32d0e98b7203fe
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-25 12:04:24 +0000

    [update]会員レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_customer!, except: [:top,:about,:index,:show]
   # before_action :authenticate_admin!, except: [:top]
-  
+
   def after_sign_out_path_for(resource)
     root_path
   end
