f725e77e708f39446a9b7498aab8a3f5ae55df2a
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-30 10:48:03 +0000

    [update]管理者側レイアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_customer!, except: [:top,:about,:index,:show]
+  # before_action :authenticate_customer!, except: [:top,:about,:index,:show]
   # before_action :authenticate_admin!, only: [:top]
 
   def after_sign_out_path_for(resource)
