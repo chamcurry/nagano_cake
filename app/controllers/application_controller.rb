7a7fe486c950e56fe8d3f97d2e410aec604c21a1
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-05-30 12:16:59 +0000

    [finish]ページ表示権限機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_customer!, except: [:top,:about,:index,:show]
-  # before_action :authenticate_admin!, only: [:top]
 
   def after_sign_out_path_for(resource)
     root_path
