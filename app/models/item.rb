634815c29995c8b6bd92fa89ebdca476d518a628
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-06-04 06:38:53 +0000

    [update]カート画面

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   has_many :cart_items
   belongs_to :genre
-  
+
   validates :image, presence: true
-  
-  def add_tax_price
-      (self.price * 1.10).round
+
+  # def add_tax_price
+  #     (self.price * 1.10).round
+  # end
+  def with_tax_price
+      (price * 1.1).floor
   end
 end
