3033a08ea07f84b101211da2972da41457031cd0
chamcurry <102650179+chamcurry@users.noreply.github.com>
2022-06-08 17:46:36 +0000

    [midway]注文情報入力

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,9 +1,13 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
   def new
+    @order =Order.new
   end
-
+  
   def confirm
+    @order =Order.new(order_params)
+    @order.save
+    redirect_to orders_confirm_path
   end
 
   def complete
@@ -16,5 +20,9 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+  private
+  def order_params
+    params.permit(:delivery_adress,:delivery_postal,:delivery_name)
   end
 end
